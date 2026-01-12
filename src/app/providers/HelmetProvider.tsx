b86dc5ffeae51d205b559027857b1fc8b1ed38a1
shamiroxs <shamiroxs@gmail.com>
2026-01-12 23:55:22 +0530

    removed route uri

--- src/app/providers/HelmetProvider.tsx
+++
@@ -1,5 +0,0 @@
-import { HelmetProvider } from "react-helmet-async";
-
-export const AppHelmetProvider = ({ children }: { children: React.ReactNode }) => (
-  <HelmetProvider>{children}</HelmetProvider>
-);
