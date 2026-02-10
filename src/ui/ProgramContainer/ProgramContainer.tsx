94890b2122da18b00ba0d787121aed355f971004
shamiroxs <shamiroxs@gmail.com>
2026-02-10 22:20:09 +0530

    removed useless code

--- src/ui/ProgramContainer/ProgramContainer.tsx
+++ src/ui/ProgramContainer/ProgramContainer.tsx
@@ -778,7 +778,7 @@ export function ProgramContainer({
       </div>
 
       {/* Arrow overlay */}
-      <ProgramArrowsOverlay isPhone={isPhone}/>
+      <ProgramArrowsOverlay key={layoutVersion} isPhone={isPhone}/>
 
       <ProgramDropzone highlight={highlightProgram}>
         <SortableContext
